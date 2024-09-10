da7364ae53ef569d8e5eb6d96401e4edc8592784
pratikkumbhar <pratikkumbhar83@gmail.com>
2024-09-10 16:08:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-screen">
+      <section className="w-full h-screen flex items-center justify-center border">
+        <div className="max-w-[800px]  bg-red-300 py-12 px-10 rounded-md">
+          <form className="">
+            <h1 className="text-red-600 mb-6 text-3xl font-bold">
+              Create
+              Account
+            </h1>
+            <div className="flex flex-col mb-5">
+              <lable className="text-red-600 text-lg font-bold">
+                Name
+              </lable>
+              <input
+                className="rounded px-2 py-1 text-lg text-semibold text-red-600 font-semibold outline-none"
+                type="text"
+              />
+            </div>{" "}
+            <div className="flex flex-col mb-5">
+              <lable className="text-red-600  text-lg font-bold">
+                Email
+              </lable>
+              <input
+                className="rounded px-2 py-1 text-lg text-semibold text-red-600 font-semibold  outline-none"
+                type="email"
+              />
+            </div>{" "}
+            <div className="flex flex-col mb-5">
+              <lable className="text-red-600  text-lg font-bold">
+                Password
+              </lable>
+              <input
+                className="rounded px-2 py-1 text-lg text-semibold text-red-600 font-semibold  outline-none"
+                type="password"
+              />
+            </div>
+            <input
+              type="submit"
+              className="bg-red-600 px-7 py-2 rounded-md font-extrabold text-white hover:scale-105 ease-in-out"
+            />
+          </form>
+        </div>
+      </section>
     </div>
   );
 }
-
-export default App;
