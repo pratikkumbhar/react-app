cbba65eebaf55b8de4193c1412a574997ab197ee
pratikkumbhar <pratikkumbhar83@gmail.com>
2024-09-18 17:10:54 +0530

    m

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,46 +1,58 @@
-import React, { useState } from "react";
+import React, {
+  useState,
+} from "react";
 import TrashLogo from "../assets/trash-solid.svg";
 import EditLogo from "../assets/pen-to-square-solid.svg";
 import "./Todo.css";
 import TodoList from "./TodoList";
 function Todo() {
-  const [list, setList] = useState([
-    { id: 1, title: "t1", task: "task" },
-    { id: 2, title: "t2", task: "task" },
-    { id: 3, title: "t3", task: "task" },
-  ]);
-  const [title, setTitle] = useState("");
-  const [task, setTask] = useState("");
-  const [edit, setEdit] = useState(false);
-  const [update, setUpdate] = useState(null);
-  const createTask = (e) => {
+  const [list, setList] =
+    useState([
+      {
+        id: 1,
+        title: "demo",
+        task: "",
+      },
+    ]);
+  const [
+    title,
+    setTitle,
+  ] = useState("");
+  const [task, setTask] =
+    useState("");
+const [edit,setEdit] = useState(false)
+  const createTask = (
+    e
+  ) => {
     e.preventDefault();
-    setList((prev) => [...prev, { id: list.length, title: title, task: task }]);
+    setList((prev) => [
+      ...prev,
+      {
+        id: list.length,
+        title: title,
+        task: task,
+      },
+    ]);
+    setTitle("");
+    setTask("");
   };
-  const updateTask = (e) => {
-    console.log("title:" + title + "task:" + task);
-    setEdit(false);
-    const prevList = [...list];
-    prevList[update.id - 1] = { id: update.id, title: title, task: task };
-    setList(prevList);
 
-    e.preventDefault();
-  };
-  const onEditClicked = (id) => {
-    if (update == null) {
-      setEdit(true);
-      const updateItem = list.filter((item) => item.id === id)[0];
-      setUpdate(updateItem);
-    } else {
-      alert("first complete prev edit");
-    }
-  };
-  const onDeleteClicked = (id) => {
-    setList(list.filter((item) => item.id !== id));
-  };
+  const onDeleteClicked =
+    (id) => {
+      setList(
+        list.filter(
+          (item,index) =>
+            index !==
+            id
+        )
+      );
+    };
   return (
     <section className="container mx-auto my-10 max-w-[1200px] h-[90vh] bg-emerald-200 rounded-xl p-7">
-      <h1 className="text-4xl font-extrabold text-emerald-900 "> TodoApp</h1>
+      <h1 className="text-4xl font-extrabold text-emerald-900 ">
+        {" "}
+        TodoApp
+      </h1>
       <div className="flex flex-row gap-10 py-10">
         <div className="bg-emerald-200 rounded-xl flex flex-col w-[35%]">
           <lable
@@ -54,9 +66,14 @@ function Todo() {
             id="input1"
             className=" h-10  rounded px-2 py-1 text-lg text-semibold bg-emerald-50 text-emerald-900 font-semibold outline-none "
             type="text"
-            defaultValue={edit ? update.title : ""}
-            onChange={(e) => {
-              setTitle(e.target.value);
+            value={title}
+            onChange={(
+              e
+            ) => {
+              setTitle(
+                e.target
+                  .value
+              );
             }}
           />
           <lable
@@ -68,51 +85,109 @@ function Todo() {
           </lable>
           <textarea
             id="input1"
-            defaultValue={edit ? update.task : ""}
-            onChange={(e) => {
-              setTask(e.target.value);
+            value={task}
+            onChange={(
+              e
+            ) => {
+              setTask(
+                e.target
+                  .value
+              );
             }}
             className=" h-[300px]  rounded px-2 py-1 text-lg text-semibold bg-emerald-50 text-emerald-900 font-semibold outline-none  "
             type="text"
           />
-          <button
-            onClick={edit ? updateTask : createTask}
+          {edit?<button
+            onClick={
+              createTask
+            }
             className="bg-emerald-700 w-[150px] h-[40px] text-emerald-50 font-extrabold text-lg rounded-md mt-7 hover:scale-95 ease-in-out duration-150"
           >
-            {edit ? "Edit" : "Create"}
-          </button>
+            Edit
+          </button>:<button
+            onClick={
+              createTask
+            }
+            className="bg-emerald-700 w-[150px] h-[40px] text-emerald-50 font-extrabold text-lg rounded-md mt-7 hover:scale-95 ease-in-out duration-150"
+          >
+            Create
+          </button>}
+          
         </div>
         <div className="flex flex-col gap-2 w-[50%]">
-          <h1 className="text-emerald-800 font-semibold text-lg">Todos</h1>
-          {list.map((value, index) => {
-            return (
-              <div
-                key={index}
-                className="bg-emerald-50 w-[100%] rounded py-4 px-7 flex justify-between  items-center"
-              >
-                <div>
-                  <h1 className="text-lg font-extrabold text-emerald-900">
-                    {value.title}
-                  </h1>
-                  <p className="text-base  text-emerald-700">{value.task}</p>
-                </div>
-                <div className="flex gap-6 h-6">
-                  <img
-                    src={TrashLogo}
-                    alt="not found"
-                    className="h-6 hover:scale-95 duration-200 ease-in-out"
-                    onClick={() => onDeleteClicked(value.id)}
-                  />
-                  <img
-                    src={EditLogo}
-                    alt="not found"
-                    onClick={() => onEditClicked(value.id)}
-                    className="h-6 hover:scale-95 duration-200 ease-in-out"
-                  />
-                </div>
-              </div>
-            );
-          })}
+          <h1 className="text-emerald-800 font-extrabold text-xl">
+            Todos
+          </h1>
+          {list.length >
+          0 ? (
+            list.map(
+              (
+                value,
+                index
+              ) => {
+                return (
+                  <div
+                    key={
+                      index
+                    }
+                    className="bg-emerald-50 w-[100%] rounded py-4 px-7 flex justify-between  items-center"
+                  >
+                    <div>
+                      <h1 className="text-lg font-extrabold text-emerald-900">
+                        {
+                          value.title
+                        }
+                      </h1>
+                      <p className="text-base  text-emerald-700">
+                        {
+                          value.task
+                        }
+                      </p>
+                    </div>
+                    <div className="flex gap-6 h-6">
+                      <img
+                        src={
+                          TrashLogo
+                        }
+                        alt="not found"
+                        className="h-6 hover:scale-95 duration-200 ease-in-out"
+                        onClick={() =>
+                          onDeleteClicked(
+                            index
+                          )
+                        }
+                      />
+                      <img
+                        src={
+                          EditLogo
+                        }
+                        onClick={()=>setEdit(true)}
+                        alt="not found"
+                        className="h-6 hover:scale-95 duration-200 ease-in-out"
+                      />
+                    </div>
+                  </div>
+                );
+              }
+            )
+          ) : (
+            <h1 className="font-extrabold text-xl text-emerald-600">
+              Add some
+              tasks
+              <span className="dot1 font-extrabold text-xl text-emerald-600">
+                {" "}
+                .
+              </span>
+              <span className="dot2 font-extrabold text-xl text-emerald-600">
+                {" "}
+                .
+              </span>
+              <span className="dot3 font-extrabold text-xl text-emerald-600">
+                {" "}
+                .
+              </span>
+            </h1>
+          )}
         </div>
       </div>
     </section>
